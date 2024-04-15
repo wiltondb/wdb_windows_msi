2077f0e0efe8601a4178a5ce78b903261c7c8f0a
alex <alex@staticlibs.net>
2024-04-15 19:23:14 +0100

    7.12.1 update build, include pg_stat_statements and system_stats to preload libs

--- resources/wiltondb-setup-01.sql
+++ resources/wiltondb-setup-01.sql
@@ -7,7 +7,7 @@ ALTER SYSTEM SET log_rotation_age = '1d';
 ALTER SYSTEM SET port = :postgres_port;
 ALTER SYSTEM SET max_connections = :max_connections;
 ALTER SYSTEM SET ssl = :enable_ssl;
-ALTER SYSTEM SET shared_preload_libraries = 'babelfishpg_tds';
+ALTER SYSTEM SET shared_preload_libraries = 'babelfishpg_tds','pg_stat_statements','system_stats';
 
 CREATE USER :username WITH SUPERUSER CREATEDB CREATEROLE PASSWORD :user_password INHERIT;
 CREATE DATABASE :dbname OWNER :username;
